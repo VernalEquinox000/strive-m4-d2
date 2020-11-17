f10abce9ad13fce91e86a9759bb0e35ae0a1d65c
VernalEquinox <pieroapruzzese@gmail.com>
2020-11-17 16:26:53 +0100

    re-commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+const NavBar = (props) => {
+
+    return (
+        <div>
+    <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">
+                    {props.title}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#browse">Browse</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+            </Navbar>
+            </div>
+    )
+
+}
+
+export default NavBar;
