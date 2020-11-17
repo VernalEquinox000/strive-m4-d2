6233c5223bb9bbd313b5781e508145eeded8bf88
VernalEquinox <pieroapruzzese@gmail.com>
2020-11-17 16:56:47 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 import NavBar from "./components/navbar"
+//import myFooter from "./components/myfooter"
+import Welcome from "./components/welcome"
 
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <NavBar title="Strive Library" color={"red"}/>
+        <NavBar title="Strive Library" color={"red"} />
+
+        <Welcome title="our library!"/>
+        
+       
+        
         
       </>
     );
---
+++ src/components/myfooter.jsx
@@ -0,0 +1,12 @@
+ import React from "react";
+
+const myFooter = () => {
+    return (
+
+    <footer>
+        This is a footer
+    </footer>
+    )
+} 
+
+export default myFooter
